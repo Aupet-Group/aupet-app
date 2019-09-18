ec44155bb3ad991709f8f4157fa09bf715c08249
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2019-09-18 14:35:59 +0200

    wip

--- routes/users.js
+++ routes/users.js
@@ -22,8 +22,9 @@ router.get('/profile/update', checkIfLoggedIn, async (req, res, next) => {
   const { _id } = req.session.currentUser;
   try {
     const user = await User.findOne({ _id });
-    console.log(`${user.owner} ${user.keeper} are the variables`);
-    res.render('users/update', { user });
+    const owner = user.owner ? 'checked' : '';
+    const keeper = user.keeper ? 'checked' : '';
+    res.render('users/update', { user, owner, keeper });
   } catch (error) {
     next(error);
   }
@@ -34,11 +35,19 @@ router.get('/profile/update', checkIfLoggedIn, async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   const { _id } = req.session.currentUser;
   const {
-    email, name, lastName, username, phone, mobile, secondaryPhone, owner: ownerString, keeper: keeperString,
+    email,
+    name,
+    lastName,
+    username,
+    phone,
+    mobile,
+    secondaryPhone,
+    owner: ownerString,
+    keeper: keeperString,
   } = req.body;
-  const owner = ownerString ==='true';
-  const keeper = keeperString === 'true';
-  console.log(`${owner} ${keeper} are the variables`);
+
+  const owner = ownerString === 'checked';
+  const keeper = keeperString === 'checked';
   try {
     const user = await User.findByIdAndUpdate(_id, {
       email,
@@ -58,9 +67,12 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-router.get('/users', checkIfLoggedIn, async (req, res, next) => {
+
+//show all of user that are keepers
+router.get('/keepers', checkIfLoggedIn, async (req, res, next) => {
   try {
     const users = await User.find({ keeper: true });
+    
     res.render('users/users', { users });
   } catch (error) {
     next(error);
