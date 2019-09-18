d79194a6969ba9f30a2d4d8697d358a773bdd350
Juan Vicente <juanvicentevazquezg@gmail.com>
2019-09-18 07:24:08 +0200

    wip

--- routes/users.js
+++ routes/users.js
@@ -23,6 +23,7 @@ router.get('/profile/update', checkIfLoggedIn, async (req, res, next) => {
   try {
     const user = await User.findOne({ _id });
     // res.send(user)
+    console.log(user);
     res.render('users/update', { user });
   } catch (error) {
     next(error);
@@ -34,8 +35,11 @@ router.get('/profile/update', checkIfLoggedIn, async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   const { _id } = req.session.currentUser;
   const {
- email, name, lastName, username, phone, mobile, secondaryPhone 
-} = req.body;
+    email, name, lastName, username, phone, mobile, secondaryPhone,
+  } = req.body;
+  const { owner } = req.body;
+  const { keeper } = req.body;
+  console.log(`${owner} ${keeper}`);
   try {
     const user = await User.findByIdAndUpdate(_id, {
       email,
@@ -45,6 +49,8 @@ router.post('/', async (req, res, next) => {
       phone,
       mobile,
       secondaryPhone,
+      owner,
+      keeper,
     });
     req.session.currentUser = user;
     res.redirect('/profile');
@@ -55,7 +61,7 @@ router.post('/', async (req, res, next) => {
 
 router.get('/users', checkIfLoggedIn, async (req, res, next) => {
   try {
-    const users = await User.find();
+    const users = await User.find({ keeper: true });
     res.render('users/users', { users });
   } catch (error) {
     next(error);
@@ -63,12 +69,11 @@ router.get('/users', checkIfLoggedIn, async (req, res, next) => {
 });
 
 router.get('/users/:_id', checkIfLoggedIn, async (req, res, next) => {
-  console.log('detalle de un usuario que no soy yo');
   const { _id } = req.params;
   try {
-    const user = await User.findOne({ _id });
+    const users = await User.findOne({ _id });
     const enabled = false;
-    res.render('users/profile', { user, enabled });
+    res.render('users/profile', { users, enabled });
   } catch (error) {
     next(error);
   }
