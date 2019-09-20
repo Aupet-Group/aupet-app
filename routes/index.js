133d568cfb74287a54202bc42557371c66a5fc15
Juan Vicente <juanvicentevazquezg@gmail.com>
2019-09-20 15:31:43 +0200

    [feat] close #137 add searchbar without login

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 const User = require('../model/user');
+let keepers;
 
 /* GET home page. */
 // eslint-disable-next-line no-unused-vars
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
-
 // you don't need to be loged in
 
 router.post('/searchbar', async (req, res, next) => {
   const { zipcode } = req.body;
   try {
-    const keepers = await User.find({ 'address.0.zipcode': zipcode, keeper: true });
-    if (typeof keepers !== 'undefined') {
-      res.locals.currentUser.keepers = JSON.parse(JSON.stringify(keepers));
-    }
+    keepers = await User.find({ 'address.0.zipcode': zipcode, keeper: true });
     res.redirect('/list');
   } catch (error) {
     next(error);
@@ -27,14 +24,10 @@ router.post('/searchbar', async (req, res, next) => {
 // you don't need to be loged in
 router.get('/list', async (req, res, next) => {
   try {
-    if (typeof res.locals.currentUser.keepers !== 'undefined') {
-      const { keepers } = res.locals.currentUser;
-      res.render('keeperzone', { keepers });
-    } else {
-      res.render('keeperzone'); 
-}
+    res.render('keeperzone', { keepers });
   } catch (error) {
     next(error);
   }
 });
+
 module.exports = router;
