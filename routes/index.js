64e29b67dcb52310c080b33c14167a75749979b8
Juan Vicente <juanvicentevazquezg@gmail.com>
2019-09-20 08:24:19 +0200

    wip

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
 const router = express.Router();
+const Event = require('../model/event');
 
 /* GET home page. */
 // eslint-disable-next-line no-unused-vars
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/searchbar', (req, res, next) => {
+  const { zipcode } = req.body;
+  const events = Event.find({}).populate('user'.address )
 });
 
 module.exports = router;
